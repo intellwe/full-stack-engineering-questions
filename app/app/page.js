d05ecf9ee254506becc29a051f5cff895619c61d
FardinHash <iamfardin1526@gmail.com>
2024-08-06 14:17:29 +0600

    Add: Concepts as mdx with modified configs

--- app/app/page.js
+++ app/app/page.js
@@ -6,8 +6,10 @@ import MDXContent from './index.mdx';
 
 export default function Page() {
   return (
-    <MDXProvider>
-      <MDXContent />
-    </MDXProvider>
+    <div className="prose prose-lg prose-indigo mx-auto px-4 py-8">
+      <MDXProvider>
+        <MDXContent />
+      </MDXProvider>
+    </div>
   );
 }
